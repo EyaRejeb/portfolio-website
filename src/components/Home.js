010e99e6dd7c44cd183fca3071b1a3907af28abe
EyaRejeb <93796426+EyaRejeb@users.noreply.github.com>
2026-06-29 20:17:05 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaInstagram, FaFacebook, FaLinkedin, FaEnvelope, FaGithub } from 'react-icons/fa';
 import Typewriter from './Typewriter';
 import heroImage from '../assets/Eya_Rejeb.jpeg'; // Ensure your image is in src/assets
- {/*import cvFile from '../assets/REJEB_Eya_CV.pdf';   // Place your CV PDF in src/assets */}
+import cvFile from '../assets/REJEB_Eya_CV.pdf';   // Place your CV PDF in src/assets
 
 function Home() {
   return (
@@ -19,9 +19,9 @@ function Home() {
           My passion is blending art and technology to deliver outstanding results.
         </p>
         {/* The button is now an anchor that downloads the CV */}
-        {/* <a className="cta-button" href={#} download> */}
-        {/*   download my CV */}
-        {/* </a> */}
+        <a className="cta-button" href={cvFile} download>
+          download my CV
+        </a>
         <div className="social-icons">
           <a href="https://www.instagram.com/eyarejeb02/" target="_blank" rel="noopener noreferrer">
             <FaInstagram />
